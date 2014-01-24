b841b6d786ddce8e38649732ae8f47aa8446878b
rasalghul <andrueastman@gmail.com>
2014-01-24 17:21:46 +0300

    updated view for marketer create agent

--- application/views/marketer/create_user.php
+++ application/views/marketer/create_user.php
@@ -1,35 +1,51 @@
-
-<div id="wrapper">
-<div id="content">
+<div id="page-wrapper">
 <?php $this->load->view('alerts');?>
 <div id="box" class ="box">
-    	<h3><?php echo $widget_title;?></h3>
-    
+    	<div class="row">
+          <div class="col-lg-12">
+            <h3>Create Agent</h3>
+          </div>
+        </div>
+   
 <?php 
 	$data = array(
 		'id'=>'form',
-		'class'=>'form'
+		'class'=>'form-horizontal',
+		'role' => 'form'
 	);
 	echo form_open('admin/user/create_user', $data);?>
+	<div class="form-group">
+		<label for="first_name" class="col-sm-2 control-label">First name</label>
+		<div class="col-sm-6">
+			<input type = "text" class="form-control" name="first_name"/>
+		</div>
+	</div>
+	
+	<div class="form-group">
+		<label for ="last_name" class="col-sm-2 control-label">Last name</label>
+		<div class="col-sm-6">
+			<input type = "text" class="form-control" name="last_name"/>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for ="email" class="col-sm-2 control-label">email</label>
+		<div class="col-sm-6">
+			<input type = "text" class="form-control" name="email"/>
+		</div>
+	</div>
+	
+	<div class="form-group">
+		<label for ="phone" class="col-sm-2 control-label">phone</label>
+		<div class="col-sm-6">
+			<input type = "number" class="form-control" name="phone"/>
+		</div>
+	</div>	
 
-	<p>
-		<label for ="first_name">firstname</label> 
-		<input type = "text" name="first_name"/>
-	</p>
-	<p>
-		<label for ="last_name">Lastname</label>
-		<input type = "text" name="last_name"/>
-	</p>
-	<p>
-		<label for ="email">email</label> 
-		<input type = "text" name="email"/>
-	</p>
-	<p>
-		<label for ="phone"> phone</label>
-		<input type = "number" name="phone"/>
-	</p>
-
-	<p><input type = "Submit" name="Submit"></p>
+	<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-10">
+			<input type = "Submit" class="btn btn-default" value="Submit"/>
+		</div>
+	</div>
 
 	</form>
 </div>
